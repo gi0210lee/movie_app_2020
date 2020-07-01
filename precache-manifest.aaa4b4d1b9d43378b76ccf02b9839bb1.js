db75a8c6d6d9a1e99276bf62f2df8d6b536018ff
gi0210lee <gi0210lee@gmail.com>
2020-07-01 15:17:07 +0900

    Updates

---
+++ precache-manifest.aaa4b4d1b9d43378b76ccf02b9839bb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c91e306a1fcb2d325229e3ae41100ca",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "5d9859cbcd88b198111f",
+    "url": "/movie_app_2020/static/css/main.4d12b7db.chunk.css"
+  },
+  {
+    "revision": "81f88c63ad6efdfb0d6b",
+    "url": "/movie_app_2020/static/js/2.d9b12856.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.d9b12856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9859cbcd88b198111f",
+    "url": "/movie_app_2020/static/js/main.b3ebe3aa.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
